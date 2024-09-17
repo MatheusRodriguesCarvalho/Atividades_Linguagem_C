5800681e962481c894a168f3907ccd46893ea2b7
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-09-17 17:54:28 -0300

    Update the pointer exercice

--- 2024_2Semetre_Atividades/20240824_Func/atividade03/main.c
+++ 2024_2Semetre_Atividades/20240824_Func/atividade03/main.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-
-
-
-
-
-
-*/
-
 void conversor_tempo(int tempo, int * ano, int * mes, int * dia, int * hora, int * min, int * seg)
-{
-    *ano = 1;
-    *mes = 2;
-    *dia = 3;
-    *hora = 4;
-    *min = 5;
-    *seg = 6;
-}
 
 int main()
 {
@@ -33,3 +16,22 @@ int main()
 
     return 0;
 }
+
+void conversor_tempo(int tempo, int * ano, int * mes, int * dia, int * hora, int * min, int * seg)
+{
+    *ano = (int) (tempo / 31104000);
+    *mes = (int) ((tempo % 31104000) / 2592000);
+    *dia = (int) (((tempo % 31104000) % 2592000) / 86400);
+    *hora = (int) ((((tempo % 31104000) % 2592000) % 86400) / 3600);
+    *min = (int) (((((tempo % 31104000) % 2592000) % 86400) % 3600) / 60);
+    *seg = (int) (((((tempo % 31104000) % 2592000) % 86400) % 3600) % 60);
+}
+
+/*
+ano - 12 meses          31.104.000
+mes - 30 dias           2.592.000
+dia - 24 horas          86.400
+hora - 60 minutos       3.600
+minuto - 60 segundos    60
+segundo - segundo       1
+*/
