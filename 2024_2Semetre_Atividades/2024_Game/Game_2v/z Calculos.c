98b04b4844f1a07945f8d7ac947a9b7b92511df9
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-09-24 18:26:49 -0300

    Update z Calculos.c in 20240924

--- 2024_2Semetre_Atividades/2024_Game/Game_2v/z Calculos.c
+++ 2024_2Semetre_Atividades/2024_Game/Game_2v/z Calculos.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mapa_definir (char *mapa, int linha, int coluna)
+{
+    for (int y = 0; y < linha; y++) //loop no mapa, com o tamanho corrente para definir as posicoes
+    {
+        for (int x = 0; x < coluna; x++)
+        {
+            if (y == 0 || x == 0 || y == linha - 1 || x == coluna - 1)
+            {
+                *mapa = '#';
+            }
+            else if (x == 2 && y == 2)
+            {
+                *mapa = 'P';
+            }
+            else if (x == coluna - 2 && y == linha - 2)
+            {
+                *mapa = '@';
+            }
+            else
+            {
+                *mapa = ' ';
+            }
+            mapa++;
+        }
+    }
+}
 
