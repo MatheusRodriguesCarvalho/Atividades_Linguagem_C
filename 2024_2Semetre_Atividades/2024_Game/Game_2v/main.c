e3972db627fd6b88180e73cdccb7b34781561790
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-09-24 18:27:10 -0300

    Update main.c in 20240924

--- 2024_2Semetre_Atividades/2024_Game/Game_2v/main.c
+++ 2024_2Semetre_Atividades/2024_Game/Game_2v/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 #define MAP_SPACE ' '
+#define MAX_LINHA 20
+#define MAX_COLUNA 20
 
 
 int main(void)
@@ -32,7 +34,7 @@ void game_loop ()
             game_outros();
             break;
         case 'x':
-            texto_sair();
+            //texto_sair();
             break;
         default:
             printf("Opcao invalida.");
@@ -47,14 +49,29 @@ void game_loop ()
 
 void game_jogar()
 {
-    printf("Jogando");
-    texto_introducao();
+    printf("Jogando\n\n");
+    //texto_introducao();
 
     struct iJogador{
         int vidaCorrente;
         int vidaMaxima;
         int forca;
-    }
+        char escolha;
+    };
+
+    struct iJogador teste;
+
+    char jogo_mapa[MAX_COLUNA][MAX_LINHA];
+    int mapa_coluna = 30, mapa_linha = 8;
+
+    mapa_definir(&jogo_mapa, mapa_linha, mapa_coluna);
+    mapa_desenhar(&jogo_mapa, mapa_linha, mapa_coluna);
+
+    jogador_movimentacao(&teste.escolha);
+
+    printf("escolha foi: %c", teste.escolha);
+
+
 }
 
 void game_tutorial()
