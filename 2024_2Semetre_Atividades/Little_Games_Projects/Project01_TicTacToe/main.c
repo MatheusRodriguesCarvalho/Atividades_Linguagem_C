aa058fcfcc0fc89b3ad688f83deb7960e141a02a
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-10-01 18:19:19 -0300

    Updating the TicTacToe Game

--- 2024_2Semetre_Atividades/Little_Games_Projects/Project01_TicTacToe/main.c
+++ 2024_2Semetre_Atividades/Little_Games_Projects/Project01_TicTacToe/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define SIZE 4
 
 int main()
 {
@@ -14,9 +17,11 @@ void game_start()
     system("Pause");
     system("cls");
 
+    srand(time(NULL));
+
     char firstMove = ' ';
     int playing = 1;
-    char game_grid[4][4] =
+    char game_grid[SIZE][SIZE] =
     {
         {' ', '0', '1', '2'},
         {'0', '#', '#', '#' },
@@ -24,23 +29,26 @@ void game_start()
         {'2', '#', '#', '#' }
     };
 
+    who_gonna_start(&firstMove);
+
     while(playing == 1)
     {
-        who_play(&firstMove);
+        printf("\n\nObnoxius random text\n\n");
         grid_draw(game_grid);
 
-        playing = 0;
+        the_move(firstMove, game_grid);
+
+        system("Pause");
+
+        //playing = 0;
     }
-
-
-
 }
 
-void grid_draw(char *m)
+void grid_draw(char *m) //it takes the TTTGrid and draws it
 {
-    for(int x = 0; x < 4; x++)
+    for(int x = 0; x < SIZE; x++)
     {
-        for(int y = 0; y < 4; y++)
+        for(int y = 0; y < SIZE; y++)
         {
             printf("%c ", *m);
             m++;
@@ -48,19 +56,51 @@ void grid_draw(char *m)
         printf("\n");
     }
 }
-
-
-void who_play(char *choice)
+void who_gonna_start(char *choice) //it'll decide who gonna start playing the game
 {
-    while((*choice != 'h') || (*choice != 't'))
+    int a = 0;
+    while(a == 0) //ponto de melhora
     {
         printf("Chose Head(h) or Tails(t)? ");
         fflush(stdin);
-        *choice = getchar();
-        //system("cls");
-    }
+        *choice = toupper(getchar());
+        system("cls");
 
-    printf("you did it.");
+        if ( (*choice == 'H') || (*choice == 'T') )
+        {
+            a = 1;
+        }
+        else
+        {
+            printf("Wrong input. Do it again, and do it right\n");
+        }
+    }
+    a = valor_aleatorio(2);
+    printf("Who starts playing is.");
+    sleep(1);
+    printf(".");
+    sleep(1);
+    printf(".");
+    sleep(1);
+    if (a == 0)
+    {
+        printf(" The player (you)\n\n");
+        *choice = 'P';
+    }
+    else
+    {
+        printf(" Your oponente (not you)\n\n");
+        *choice = 'O';
+    }
+    system("Pause");
+}
+int valor_aleatorio(int valor_maximo)
+{
+    int probabilidade = rand() % valor_maximo;
+    return probabilidade;
 }
 
+void the_move(char first, char *grid)
+{
 
+}
