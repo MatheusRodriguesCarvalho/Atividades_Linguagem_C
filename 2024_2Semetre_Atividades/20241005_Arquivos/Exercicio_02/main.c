4c0b75970fa720961d2640d40ae9d034f9e7744e
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-10-05 13:00:02 -0300

    Addin some more 20241005

--- 2024_2Semetre_Atividades/20241005_Arquivos/Exercicio_02/main.c
+++ 2024_2Semetre_Atividades/20241005_Arquivos/Exercicio_02/main.c
@@ -11,7 +11,59 @@ desse texto.
 3 - Mostre o conteudo do arquivo original e do novo
 */
 
+void EscreverEntreLinhas()
+{
+    FILE *texto_Original;
+    FILE *texto_Novo;
+    char linha[Tamanho];
+    int contagem = 0;
 
+    texto_Original = fopen("Texto_Original.txt", "r");
+    if (texto_Original == NULL)
+    {
+        printf("Erro ao abrir o Arquivo Texto_Original.txt");
+    }
+    else
+    {
+        texto_Novo = fopen("Texto_Novo.txt", "w");
+        if (texto_Novo == NULL)
+        {
+            printf("Erro ao criar o Arquivo Texto_Novo.txt");
+        }
+        else
+        {
+            while( !feof(texto_Original) )
+            {
+                fgets(linha, Tamanho, texto_Original);
+                contagem++;
+                fputs(linha, texto_Novo);
+                if(contagem == 3)
+                {
+                    printf("Digite um nola linha: ");
+                    fflush(stdin);
+                    fgets(linha, Tamanho, stdin);
+                    fputs(linha, texto_Novo);
+                }
+            }
+            fputs(linha, texto_Novo);
+
+            fclose(texto_Novo);
+        }
+        fclose(texto_Original);
+    }
+}
+
+int main()
+{
+    EscreverEntreLinhas();
+    //criarCopia(); // por Strings
+    //escreverLinha();
+    //compararTextos();
+
+    return 0;
+}
+
+/*
 
 void criarCopia()
 {
@@ -27,15 +79,19 @@ void criarCopia()
     else
     {
         texto_Novo = fopen("Texto_Novo.txt", "w");
-        if ()
+        if (texto_Novo == NULL)
         {
             printf("Erro ao criar o Arquivo Texto_Novo.txt");
         }
         else
         {
-
-
-
+            fgets(linha, Tamanho, texto_Original);
+            while( !feof(texto_Original))
+            {
+                fputs(linha, texto_Novo); // fputs(" teste ", texto_Novo);
+                fgets(linha, Tamanho, texto_Original);
+            }
+            fputs(linha, texto_Novo);
 
             fclose(texto_Novo);
         }
@@ -43,11 +99,29 @@ void criarCopia()
     }
 }
 
-int main()
+void escreverLinha()
 {
-    criarCopia(); // por Strings
-    //escreverLinha();
-    //compararTextos();
+    FILE *texto_Novo;
+    char linha[Tamanho];
 
-    return 0;
+    texto_Novo = fopen("Texto_Novo.txt", "r+");
+    if (texto_Novo == NULL)
+    {
+        printf("Erro ao abrir o Arquivo Texto_Novo.txt");
+    }
+    else
+    {
+        fgets(linha, Tamanho, stdin);
+        for (int i = 1; !feof(texto_Novo); i++)
+        {
+            if (i == 3)
+            {
+                fputs(linha, texto_Novo);
+            }
+        }
+
+        fclose(texto_Novo);
+    }
 }
+
+*/
