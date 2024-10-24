03b32fa271987b12ecc82baf597e06c5a3401079
Matheus Rodrigues Carvalho <MATHEUS.RC808@GMAIL.COM>
2024-10-24 18:31:37 -0300

    Update Batalha_Naval.c

--- 2024_2Semetre_Atividades/2024_02S_JogoProgII/Batalha_Naval.c
+++ 2024_2Semetre_Atividades/2024_02S_JogoProgII/Batalha_Naval.c
@@ -1,13 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <string.h>
+
+/*
+
+IFORMAÇÃO DOS BARCOS
+
+1. VETOR:
+
+n = ??; //varia de acordo com a dificuldade
+tpEmbarcacao Barcos_Player[n];
+tpEmbarcacao Barcos_Enemy[n];
+
+funcaoQueDivideAQuantidadeDeBarcosParaCadaUm();
+funcaoQuePermitePersonalizarAQuantidade()
+
+*/
+
+typedef struct {int x; int y; int tamanho; char orientacao; } tpEmbacacao;
+
+void GerarArquivo()
+{
+    //a
+}
+
+void GerarEmbarcacoes(int QuantidadeBarcos, char *Jogador, char *Inimigo)
+{
 
 
 
 
+    tpEmbacacao BarcosJogador[QuantidadeBarcos];
+    tpEmbacacao BarcosInimigo[QuantidadeBarcos];
 
 
 
+    GerarArquivo(&BarcosJogador, &BarcosInimigo);
+}
+
+void GerarMapa(char metodo, char dificuldade)
+{
+    int tamanho = 0, QuantidadeBarcos  = 0;
+    switch (dificuldade)
+    {
+    case 'A':
+        tamanho = 7;
+        QuantidadeBarcos = 3;
+        break;
+    case 'B':
+        tamanho = 7;
+        QuantidadeBarcos = 3;
+        break;
+    case 'C':
+        tamanho = 7;
+        QuantidadeBarcos = 3;
+        break;
+    }
+    char MapaJogador[tamanho][tamanho];
+    char MapaInimigo[tamanho][tamanho];
+
+    GerarEmbarcacoes(QuantidadeBarcos, MapaJogador, MapaInimigo);
+
+}
+
+void Configurar()
+{
+    char alocacao, dificuldade;
+
+    printf("\nRealocando as posisoes dos Navios...");
+    do{
+        printf("\nDeseja Aloacar Manualmente (M) ou Automaticamente (A)?");
+        fflush(stdin);
+        scanf("%c", &alocacao);
+        alocacao = Toupper(alocacao);
+        system("cls");
+        //condicional
+    }while (alocacao != 'M' && alocacao != 'A');
+    do{
+        printf("\nA - Facil (7x7, 3 Barcos)");
+        printf("\nB - Medio (11x11, 5 Barcos)");
+        printf("\nC - Dificil (15x15, 8 Barcos)");
+        printf("\nZ - Detalhamento...");
+        printf("\nEscolha a Difiuldade:");
+        fflush(stdin);
+        scanf("%C", &dificuldade);
+        dificuldade = Toupper(dificuldade);
+        system("cls");
+        //condicional
+    }while (dificuldade != 'A' && dificuldade != 'B' && dificuldade != 'C');
+
+    GerarMapa(alocacao, dificuldade);
+
+}
 void Menu()
 {
     int opcao = 0;
@@ -20,14 +105,14 @@ void Menu()
         printf("\n3 - Como Jogar");
         printf("\n4 - Sair");
 
-        printf("Digite o numero da Opcao:");
+        printf("\nDigite o numero da Opcao:");
         fflush(stdin);
         scanf("%i", &opcao);
 
         switch(opcao)
         {
         case 1:
-            //funcao
+            Configurar();
             break;
         case 2:
             //funcao
@@ -35,7 +120,9 @@ void Menu()
         case 3:
             //funcao
             break;
-        default
+        case 4:
+            break;
+        default:
             printf("\nOpcao invalida!");
             break;
         }
@@ -43,13 +130,6 @@ void Menu()
         getchar();
     }
 }
-
-
-
-
-
-
-
 int main (void)
 {
     Menu();
